aada548cde3923ada13464d207f876d71a3908c9
leandrop <leandropennella@yahoo.com.ar>
2015-10-28 09:01:20 -0300

    1028 | Usuario > Controlador Login

---
+++ tphibernate/src/ar/uces/web2/tphibernate/controllers/UsuarioController.java
@@ -0,0 +1,8 @@
+package ar.uces.web2.tphibernate.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsuarioController {
+
+}
---
+++ tphibernate/src/ar/uces/web2/tphibernate/model/base/Usuario.java
@@ -0,0 +1,13 @@
+package ar.uces.web2.tphibernate.model.base;
+
+public class Usuario {
+	
+	private int id;
+	private String nombreUsuario;
+	private String nombre;
+	private String apellido;
+	private String contrasenia;
+	private String lenguaje;
+	
+
+}
