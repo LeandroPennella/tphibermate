2aab5c6322233cc2c84e22348c96d409520a839c
leandrop <leandropennella@yahoo.com.ar>
2015-11-09 14:28:04 -0300

    1109|MapeoTareas2

--- tphibernate/src/ar/edu/uces/web2/tphibernate/modelo/base/Reunion.java
+++ tphibernate/src/ar/edu/uces/web2/tphibernate/modelo/base/Reunion.java
@@ -1,9 +1,13 @@
 package ar.edu.uces.web2.tphibernate.modelo.base;
 
+import java.util.List;
+
 public class Reunion extends Evento {
 
 	private String temario;
 	private Sala sala;
+	private List<Usuario> participantes;
+	
 	public Reunion() {
 		super();
 	}
@@ -19,5 +23,10 @@ public class Reunion extends Evento {
 	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
-	
+	public List<Usuario> getParticipantes() {
+		return participantes;
+	}
+	public void setParticipantes(List<Usuario> participantes) {
+		this.participantes = participantes;
+	}
 }
