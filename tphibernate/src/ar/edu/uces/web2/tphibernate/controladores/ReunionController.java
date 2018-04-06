b210927e866290349b516b431ff96532ee53543a
leandropennella <leandropennella@envy>
2018-04-06 19:56:09 -0300

    separe save

--- tphibernate/src/ar/edu/uces/web2/tphibernate/controladores/ReunionController.java
+++ tphibernate/src/ar/edu/uces/web2/tphibernate/controladores/ReunionController.java
@@ -74,6 +74,16 @@ public class ReunionController {
 	@Autowired
 	private EventoFormValidator eventoFormValidator; 
 	
+	
+
+	@RequestMapping(value = "/calendario/listadoDeUsuarios", method = RequestMethod.GET)
+	public @ResponseBody List<Usuario> listadoDeUsuarios (String parteNombre)
+	{
+		return usuarioDAO.find(parteNombre);			
+	}
+	
+	
+	
 	@RequestMapping(value = "/calendario/crearReunion")
 	public ModelAndView crear(@ModelAttribute("usuarioLogueado") Usuario usuarioLogueado) {
 		
@@ -172,117 +182,126 @@ public class ReunionController {
 		return new ModelAndView("/views/calendario/reunion.jsp","reunionForm", reunionForm);
 	}
 		
-	@RequestMapping(value = "/calendario/agregarReunion")
+	@RequestMapping(value = "/calendario/agregarReunion")//TODO: modificar url
 	public ModelAndView save(@ModelAttribute("reunionForm") ReunionForm reunionForm, BindingResult result, @ModelAttribute("usuarioLogueado") Usuario usuarioLogueado ) {
 
 		this.eventoFormValidator.validate(reunionForm, result);
 		this.reunionValidator.validate(reunionForm, result);
 		if (result.hasErrors()) {		//tiene errores
-			//TODO: Agregarle salas y usuarios
-
-			if (!reunionForm.getIdEvento().isEmpty())  { //en modificacion
-				Reunion reunion=reunionDAO.get(Long.parseLong(reunionForm.getIdEvento()));
-				reunionForm.setInvitaciones(reunion.getInvitaciones());
-				/*
-				 * en desuso por autocomplete + tablainvitaciones
-				 
-				Map<Usuario,Integer> mapaUsuariosMasConfirmacion=new TreeMap<Usuario,Integer>();			
-				List<Usuario> usuarios=usuarioDAO.getAll();//toma los usuarios en las invitaciones hechas y los marca como ya invitados
-				for(Usuario usuario: usuarios)
-				{	
-					if (usuario.getId()!=usuarioLogueado.getId()){
-						int idConfirmacion=-1;
-						for(String tokenInvitadoMasConfirmacion:reunionForm.getTokensInvitadosMasConfirmacion()) 			
-						{
-							StringTokenizer invitacionTokenizer=new StringTokenizer(tokenInvitadoMasConfirmacion,"|");
-							int idUsuario=Integer.parseInt(invitacionTokenizer.nextToken());
-							//int idConfirmacion=Integer.parseInt(invitacionTokenizer.nextToken());
-
-							if ((idUsuario==usuario.getId())) {
-								idConfirmacion=Integer.parseInt(invitacionTokenizer.nextToken());
-							}
-						}
-						mapaUsuariosMasConfirmacion.put(usuario, idConfirmacion);			
-					}
-				}
-				reunionForm.setMapaUsuariosMasConfirmacion(mapaUsuariosMasConfirmacion);							//todos los usuarios, los invitados con idConfirmacion, los que no con -1
-				*/
-			} else { 																//errores en creacion
-				/*
-				 * en desuso autocomplete + tablainvitaciones
-				Map<Usuario,Integer> mapaUsuariosMasConfirmacion=new TreeMap<Usuario,Integer>();			
-				List<Usuario> usuarios=usuarioDAO.getAll();													
-				for(Usuario usuario: usuarios)
-				{	
-					if (usuario.getId()!=usuarioLogueado.getId()){
-						mapaUsuariosMasConfirmacion.put(usuario, -1);
-					}
-				}
-				reunionForm.setMapaUsuariosMasConfirmacion(mapaUsuariosMasConfirmacion);
-				*/	
-			}
-			
-			reunionForm.setSalas(salaDAO.getAll());
-			reunionForm.setIdEvento(null);
-			return new ModelAndView("/views/calendario/reunion.jsp","reunionForm", reunionForm);
+			return new ModelAndView("/views/calendario/reunion.jsp","reunionForm", save_conError(reunionForm));
 		} else {																							//no tiene errores
-			Reunion reunion;
-			if (!reunionForm.getIdEvento().isEmpty())  { //modificar
-				reunion=reunionDAO.get(Long.parseLong(reunionForm.getIdEvento()));
-			}  else  {									//crear
-				reunion=new Reunion();
-				reunion.setAutor(usuarioLogueado);
-			}
-
-			reunion.setTitulo(reunionForm.getTitulo());
-			SimpleDateFormat dateFormatter=new SimpleDateFormat("dd/MM/yyyy");
-			Date fecha=dateFormatter.parse(reunionForm.getFecha(), new ParsePosition(0));	
-			reunion.setFecha(fecha);
-			reunion.setHoraInicio(reunionForm.getHoraInicio());
-			reunion.setHoraFin(reunionForm.getHoraFin());
-			reunion.setTemario(reunionForm.getTemario());
-			Sala sala=new Sala();
-			sala.setId(reunionForm.getIdSala());
-			reunion.setSala(sala);
-			
-			
-
-			//TODO: agrega nuevos, no modifica
-			//for(Integer idInvitado:reunionForm.getIdsInvitados())//{listaParticipantes.addAll(new Usuario(){id=idParticipante}}	
-
-			
-			//List<String> tokensInvitadosMasConfirmacion=new ArrayList<String>();							//lista de pares idUsuario|confirmacion elegidoen el formulario//todos los usuarios posibles, y seteados los agregados //TODO: Ajax: listar solo los usuarios que no estar invitados
-			Set<Invitacion>invitaciones=new HashSet<Invitacion>();
-			/*
-			for(String tokenInvitadoMasConfirmacion:reunionForm.getTokensInvitadosMasConfirmacion())
-			{
-				StringTokenizer invitacionTokenizer=new StringTokenizer(tokenInvitadoMasConfirmacion,"|");
-				
-				int idUsuario=Integer.parseInt(invitacionTokenizer.nextToken());
-				int idConfirmacion=Integer.parseInt(invitacionTokenizer.nextToken());
-				if (idUsuario!=0){
-					Invitacion invitacion=new Invitacion();
-					
-					Usuario usuarioInvitado=new Usuario();
-					usuarioInvitado.setId(idUsuario);
-					invitacion.setUsuario(usuarioInvitado);													//TODO: Contructor de usuario
-					
-					if (idConfirmacion!=-1){invitacion.setAceptado(idConfirmacion);}
-					invitacion.setReunion(reunion);	//TODO: SolucionInvitados
-					invitaciones.add(invitacion);
-				}
-			}
-			*/
-			if (reunion.getInvitaciones()==null){
-				reunion.setInvitaciones(invitaciones);
-			} else {
-				reunion.getInvitaciones().clear();
-				reunion.getInvitaciones().addAll(invitaciones);
-			}
-				
-			reunionDAO.save(reunion);
+			save_sinError(reunionForm, usuarioLogueado);
 			return new ModelAndView("/views/index.jsp");
 		}
+	}
+	private ReunionForm save_conError(ReunionForm reunionForm) {
+		//TODO: Agregarle salas y usuarios
+
+		if (!reunionForm.getIdEvento().isEmpty())  { //en modificacion
+			Reunion reunion=reunionDAO.get(Long.parseLong(reunionForm.getIdEvento()));
+			reunionForm.setInvitaciones(reunion.getInvitaciones());
+			/*
+			 * en desuso por autocomplete + tablainvitaciones
+			 
+			Map<Usuario,Integer> mapaUsuariosMasConfirmacion=new TreeMap<Usuario,Integer>();			
+			List<Usuario> usuarios=usuarioDAO.getAll();//toma los usuarios en las invitaciones hechas y los marca como ya invitados
+			for(Usuario usuario: usuarios)
+			{	
+				if (usuario.getId()!=usuarioLogueado.getId()){
+					int idConfirmacion=-1;
+					for(String tokenInvitadoMasConfirmacion:reunionForm.getTokensInvitadosMasConfirmacion()) 			
+					{
+						StringTokenizer invitacionTokenizer=new StringTokenizer(tokenInvitadoMasConfirmacion,"|");
+						int idUsuario=Integer.parseInt(invitacionTokenizer.nextToken());
+						//int idConfirmacion=Integer.parseInt(invitacionTokenizer.nextToken());
+
+						if ((idUsuario==usuario.getId())) {
+							idConfirmacion=Integer.parseInt(invitacionTokenizer.nextToken());
+						}
+					}
+					mapaUsuariosMasConfirmacion.put(usuario, idConfirmacion);			
+				}
+			}
+			reunionForm.setMapaUsuariosMasConfirmacion(mapaUsuariosMasConfirmacion);							//todos los usuarios, los invitados con idConfirmacion, los que no con -1
+			*/
+		} else { 																//errores en creacion
+			/*
+			 * en desuso autocomplete + tablainvitaciones
+			Map<Usuario,Integer> mapaUsuariosMasConfirmacion=new TreeMap<Usuario,Integer>();			
+			List<Usuario> usuarios=usuarioDAO.getAll();													
+			for(Usuario usuario: usuarios)
+			{	
+				if (usuario.getId()!=usuarioLogueado.getId()){
+					mapaUsuariosMasConfirmacion.put(usuario, -1);
+				}
+			}
+			reunionForm.setMapaUsuariosMasConfirmacion(mapaUsuariosMasConfirmacion);
+			*/	
+		}
+		
+		reunionForm.setSalas(salaDAO.getAll());
+		reunionForm.setIdEvento(null);
+		return reunionForm;
+
+	}
+	private void save_sinError(ReunionForm reunionForm, Usuario usuarioLogueado) {
+		
+		Reunion reunion;
+		if (!reunionForm.getIdEvento().isEmpty())  { //modificar
+			reunion=reunionDAO.get(Long.parseLong(reunionForm.getIdEvento()));
+		}  else  {									//crear
+			reunion=new Reunion();
+			reunion.setAutor(usuarioLogueado);
+		}
+
+		reunion.setTitulo(reunionForm.getTitulo());
+		SimpleDateFormat dateFormatter=new SimpleDateFormat("dd/MM/yyyy");
+		Date fecha=dateFormatter.parse(reunionForm.getFecha(), new ParsePosition(0));	
+		reunion.setFecha(fecha);
+		reunion.setHoraInicio(reunionForm.getHoraInicio());
+		reunion.setHoraFin(reunionForm.getHoraFin());
+		reunion.setTemario(reunionForm.getTemario());
+		Sala sala=new Sala();
+		sala.setId(reunionForm.getIdSala());
+		reunion.setSala(sala);
+		
+		
+
+		//TODO: agrega nuevos, no modifica
+		//for(Integer idInvitado:reunionForm.getIdsInvitados())//{listaParticipantes.addAll(new Usuario(){id=idParticipante}}	
+
+		
+		//List<String> tokensInvitadosMasConfirmacion=new ArrayList<String>();							//lista de pares idUsuario|confirmacion elegidoen el formulario//todos los usuarios posibles, y seteados los agregados //TODO: Ajax: listar solo los usuarios que no estar invitados
+		Set<Invitacion>invitaciones=new HashSet<Invitacion>();
+		//TODO: revisar invitados > si no estaban agregar,  si estaban y no estan, quitarlos
+		/*
+		for(String tokenInvitadoMasConfirmacion:reunionForm.getTokensInvitadosMasConfirmacion())
+		{
+			StringTokenizer invitacionTokenizer=new StringTokenizer(tokenInvitadoMasConfirmacion,"|");
+			
+			int idUsuario=Integer.parseInt(invitacionTokenizer.nextToken());
+			int idConfirmacion=Integer.parseInt(invitacionTokenizer.nextToken());
+			if (idUsuario!=0){
+				Invitacion invitacion=new Invitacion();
+				
+				Usuario usuarioInvitado=new Usuario();
+				usuarioInvitado.setId(idUsuario);
+				invitacion.setUsuario(usuarioInvitado);													//TODO: Contructor de usuario
+				
+				if (idConfirmacion!=-1){invitacion.setAceptado(idConfirmacion);}
+				invitacion.setReunion(reunion);	//TODO: SolucionInvitados
+				invitaciones.add(invitacion);
+			}
+		}
+		*/
+		if (reunion.getInvitaciones()==null){
+			reunion.setInvitaciones(invitaciones);
+		} else {
+			reunion.getInvitaciones().clear();
+			reunion.getInvitaciones().addAll(invitaciones);
+		}
+			
+		reunionDAO.save(reunion);
 	}
 	
 	@RequestMapping(value = "/calendario/eliminarReunion")
@@ -292,18 +311,4 @@ public class ReunionController {
 		return new ModelAndView("/views/index.jsp");
 	}
 
-	@RequestMapping(value = "/calendario/listadoDeUsuarios", method = RequestMethod.GET)
-	public @ResponseBody List<Usuario> listadoDeUsuarios (String parteNombre)
-	{
-		return usuarioDAO.find(parteNombre);	
-		//return new ArrayList<String>(Arrays.asList("pepe", "pablo", "La Plata"));
-		
-	}
-	
-	@RequestMapping(value = "/calendario/autocomplete")
-	public ModelAndView autocomplete() {
-		
-		return new ModelAndView("/views/calendario/pruebas/autocomplete.jsp");
-		
-	}
 }
