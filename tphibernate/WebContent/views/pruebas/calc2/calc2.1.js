1772c2b560aa94f7124091ec6280435f2685a17a
leandropennella <leandropennella@yahoo.com.ar>
2018-06-07 10:31:03 -0300

    nav lat

---
+++ tphibernate/WebContent/views/pruebas/calc2/calc2.1.js
@@ -0,0 +1,34 @@
+//96vw>100vw-(2*2vw)
+$(function() {
+  alternar();
+})
+$("#toggleBoton").click(function() {
+  alternar();
+});
+
+function alternar() {
+  $('#mySidenav').toggleClass('active');
+  if (!$('#mySidenav').hasClass('active')) {
+    $('#mySidenav').css('width', '250px');
+    $('#main').css('margin-left', '250px');
+
+    /*$('[class^="mitable"]').css('width', 'calc(96vw-250px)');*/
+    /*$('.mitable').css('width', 'calc(96vw-250px)');*/
+    $('.calendario-cabecera').css('width', 'calc((96vw - 250px) + 1px)'); /*1847.4*/
+    $('.calendario-cuerpo').css('width', 'calc((96vw - 250px) + 0.5vw + 0.5px)'); //96+0.5>scrollbar
+    $('.columnas-horas').css('width', 'calc((96vw - 250px)/36)');
+    $('.columnas-dias').css('width', 'calc(((96vw - 250px)/36)*5)');
+    //calc((100vw-250px)/36)
+    //calc(((100vw-250px)/36)*5)
+
+  } else {
+    $('#mySidenav').css('width', '0px');
+    $('#main').css('margin-left', '0px');
+    $('.calendario-cabecera').css('width', 'calc(96vw + 1px)'); /*1847.4*/
+    $('.calendario-cuerpo').css('width', 'calc(96.5vw + 0.5px)'); //96+0.5>scrollbar
+    $('.columnas-horas').css('width', 'calc(96vw/36)');
+    $('.columnas-dias').css('width', 'calc((96vw/36)*5)');
+    //calc((100vw-8px)/36)
+    //calc(((100vw - 8px)/36)*5)";
+  }
+}
