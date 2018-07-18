07b409839963e641ee0dbba6ac71f5b873b235c8
leandro <leandro@maquinitaBudgie>
2018-07-18 19:39:49 -0300

    fecha semana

--- tphibernate/src/ar/edu/uces/web2/tphibernate/controladores/EventoController.java
+++ tphibernate/src/ar/edu/uces/web2/tphibernate/controladores/EventoController.java
@@ -23,7 +23,9 @@ import ar.edu.uces.web2.tphibernate.modelo.base.Usuario;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
 
@@ -54,13 +56,69 @@ public class EventoController {
 		return eventoDAO.mover(evento);
 		 
 	}
+
 	
-	/*
-	@RequestMapping(value = "/evento/mover")
-	public @ResponseBody void moverEvento(HttpServletRequest request, HttpServletResponse response, @RequestParam(value="origenId", required=true) Integer origenId, @RequestParam(value="destinoId", required=true) String destinoId) //@ModelAttribute("usuarioLogueado") Usuario usuarioLogueado
-	{
-		//return void;
+	public int diferenciaSemanas(Date fechaDestino) {
+		
+		 
+		Date fechaActual=new Date();
+ 
+		int dias=(int) ((fechaActual.getTime()-fechaDestino.getTime())/86400000);
+		int semanas=dias/7;
+		
 	}
-	*/
+	
+	
+	public int difSem() {
+		Calendar actual = new GregorianCalendar();
+        Calendar destino = new GregorianCalendar();
+        try {
+			actual.setTime(new SimpleDateFormat("dd/MM/yyyy").parse("01/05/2017"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        destino.setTime(new SimpleDateFormat("dd/MM/yyyy").parse("01/08/2018"));
+        int difA = destino.get(Calendar.YEAR) - actual.get(Calendar.YEAR);
+        int difM = difA * 12 + destino.get(Calendar.MONTH) - actual.get(Calendar.MONTH);
+        int difS;
+        if(difA==0) {
+        	difS=destino.get(Calendar.WEEK_OF_YEAR)-actual.get(Calendar.WEEK_OF_YEAR);
+		} else {}
+	}
+	
+	public long difDiasEntre2fechas(int Y1,int M1,int D1,int Y2,int M2,int D2){
+		
+		
+		java.util.Date fecha = new Date();
+
+		
+		
+		dia = Integer.toString(c.get(Calendar.DATE));
+		mes = Integer.toString(c.get(Calendar.MONTH));
+		annio = Integer.toString(c.get(Calendar.YEAR));
+		
+		java.util.GregorianCalendar date=new java.util.GregorianCalendar(Y1,M1,D1);
+		java.util.GregorianCalendar date2=new java.util.GregorianCalendar(Y2,M2,D2);
+		long difms=date2.getTimeInMillis() - date1.getTimeInMillis();
+		long difd=difms / (1000 * 60 * 60 * 24);
+		return difd;
+		}
+	
+	public int semanasOffset(Date fecha){
+		int semanasOffset=0;
+		//establecer en que semana del anio que estoy
+		Calendar calendar = Calendar.getInstance();
+		int semanaActual=Calendar.WEEK_OF_YEAR;
+		
+		calendar.setTime(fecha);
+		int semanaDestino=Calendar.WEEK_OF_YEAR;
+		
+		//establecer la semana del anio que esta la fecha
+		//si es el mismo anio devolver semanaActual-semanaDestino
+		//si no es el mismo anio
+		return semanasOffset;
+		
+	}
 	
 }
